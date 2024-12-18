b7469883fd2b1a8458869b711e41e179acd4fe08
V-Karch <luna.karch05@gmail.com>
2024-12-17 21:44:21 -0500

    general structure: implemented clap for cli parsing

---
+++ src/cli_parse.rs
@@ -0,0 +1,16 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None, disable_help_flag = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    ListSupported,
+    Search { title: Vec<String> },
+    Add { id: u32, name: Vec<String> },
+    Download { name: Vec<String> },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,114 @@
-mod utils;
 use tokio;
+use clap::Parser;
+
+mod cli_parse;
+mod utils;
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
-    if args.len() == 0 {
-        utils::display_help_message();
-        return;
-    }
-
-    let parsed_argument = args[0].to_ascii_lowercase().replace(" ", "-");
+    let cli = cli_parse::Cli::parse();
 
     let pool = utils::connect()
         .await
         .expect("Failed to load database pool");
 
-    if parsed_argument == "list-supported" {
-        let supported = utils::get_supported_titles(&pool).await;
-        println!("{}Supported titles:{}", utils::BLUE, utils::RESET);
-        println!("{}{}{}", utils::WHITE, supported.join(", "), utils::RESET);
-        println!(
-            "\n{}{} total supported titles.{}",
-            utils::GREEN,
-            supported.len(),
-            utils::RESET
-        );
-        return;
-    }
-
-    if parsed_argument == "search" && args.len() >= 2 {
-        let search_title = args[1..].join("-").to_ascii_lowercase();
-
-        println!(
-            "{}Searching for title {}`{}`{}...{}",
-            utils::BLUE,
-            utils::WHITE,
-            search_title,
-            utils::BLUE,
-            utils::RESET
-        );
-
-        let results = utils::search_titles_by_name(&pool, &search_title).await;
-
-        println!("{}Top 10 Related Results:{}\n", utils::BLUE, utils::RESET);
-        println!("{}{}{}", utils::WHITE, results.join(", "), utils::RESET);
-
-        return;
-    }
-
-    if parsed_argument == "add" && args.len() >= 2 {
-        let title_id = match args[1].parse::<u32>() {
-            Ok(value) => value,
-            Err(_) => {
-                println!("{}When adding to the title database, the second argument must be an {}integer id{} for the title, not {}`{}`{}", 
-                utils::BLUE, utils::WHITE, utils::BLUE, utils::WHITE, &args[1], utils::RESET
+    match &cli.command {
+        Some(cli_parse::Commands::ListSupported) => {
+            let supported = utils::get_supported_titles(&pool).await;
+            println!("{}Supported titles:{}", utils::BLUE, utils::RESET);
+            println!("{}{}{}", utils::WHITE, supported.join(", "), utils::RESET);
+            println!(
+                "\n{}{} total supported titles.{}",
+                utils::GREEN,
+                supported.len(),
+                utils::RESET
             );
+        }
+        Some(cli_parse::Commands::Search { title }) => {
+            let search_title = title.join("-").to_ascii_lowercase();
+            println!(
+                "{}Searching for title {}`{}`{}...{}",
+                utils::BLUE,
+                utils::WHITE,
+                search_title,
+                utils::BLUE,
+                utils::RESET
+            );
+
+            let results = utils::search_titles_by_name(&pool, &search_title).await;
+
+            println!("{}Top 10 Related Results:{}\n", utils::BLUE, utils::RESET);
+            println!("{}{}{}", utils::WHITE, results.join(", "), utils::RESET);
+        }
+        Some(cli_parse::Commands::Add { id, name }) => {
+            let title = name.join("-");
+            println!(
+                "{}Attempting to add title {}`{}`{} with {}id `{}`{}",
+                utils::BLUE,
+                utils::WHITE,
+                title,
+                utils::BLUE,
+                utils::WHITE,
+                id,
+                utils::RESET
+            );
+
+            let add_result = match utils::add_title(*id, &title, &pool).await {
+                Ok(value) => value,
+                Err(_) => -1,
+            };
+
+            if add_result >= 1 {
+                println!(
+                    "{}Added title {}`{}`{} to the database with {}id `{}`{}",
+                    utils::BLUE,
+                    utils::WHITE,
+                    title,
+                    utils::BLUE,
+                    utils::WHITE,
+                    id,
+                    utils::RESET
+                );
+            } else {
+                println!(
+                    "{}Could not add title {}`{}`{} to database with {}id `{}`{}",
+                    utils::GREEN,
+                    utils::WHITE,
+                    title,
+                    utils::GREEN,
+                    utils::WHITE,
+                    id,
+                    utils::RESET
+                );
+            }
+        }
+        Some(cli_parse::Commands::Download { name }) => {
+            let title_name = name.join("-").to_ascii_lowercase();
+            let title_id = utils::get_title_id(&title_name, &pool).await;
+
+            if title_id < 0 {
+                println!("Title `{}` was not found in the database", &title_name);
                 return;
             }
-        };
 
-        let title = &args[2..].join("-");
-        println!(
-            "{}Attempting to add title {}`{}`{} with {}id `{}`{}",
-            utils::BLUE,
-            utils::WHITE,
-            title,
-            utils::BLUE,
-            utils::WHITE,
-            title_id,
-            utils::RESET
-        );
+            let base_text = reqwest::get(&format!("https://hshop.erista.me/t/{}", &title_id))
+                .await
+                .expect("Failed to make initial request")
+                .text()
+                .await
+                .expect("Failed to parse request text")
+                .lines()
+                .map(|f| f.to_string())
+                .collect::<Vec<String>>();
 
-        let add_result = match utils::add_title(title_id, &title, &pool).await {
-            Ok(value) => value,
-            Err(_) => -1,
-        };
-
-        if add_result >= 1 {
-            println!(
-                "{}Added title {}`{}`{} to the databae with {}id `{}`{}",
-                utils::BLUE,
-                utils::WHITE,
-                title,
-                utils::BLUE,
-                utils::WHITE,
-                title_id,
-                utils::RESET
-            );
-        } else {
-            println!(
-                "{}Could not add title {}`{}`{} to database with {}id `{}`{}",
-                utils::GREEN,
-                utils::WHITE,
-                title,
-                utils::GREEN,
-                utils::WHITE,
-                title_id,
-                utils::RESET
-            )
+            let request_url = utils::extract_url(&base_text);
+            println!("Requesting URL `{}`...", request_url);
+            if let Err(e) = utils::download_with_progress(&request_url, &title_name).await {
+                eprintln!("Error during download: {}", e);
+            }
         }
-
-        return;
-    }
-
-    let title_id = utils::get_title_id(&parsed_argument, &pool).await;
-
-    if title_id < 0 {
-        println!("Title `{}` was not found in the database", &parsed_argument);
-        return;
-    }
-
-    let base_text = reqwest::get(&format!("https://hshop.erista.me/t/{}", &title_id))
-        .await
-        .expect("Failed to make initial request")
-        .text()
-        .await
-        .expect("Failed to parse request text")
-        .lines()
-        .map(|f| f.to_string())
-        .collect::<Vec<String>>();
-
-    let request_url = utils::extract_url(&base_text);
-    println!("Requesting URL `{}`...", request_url);
-    if let Err(e) = utils::download_with_progress(&request_url, &parsed_argument).await {
-        eprintln!("Error during download: {}", e);
+        None => {
+            utils::display_help_message();
+        }
     }
 }
